7443a891af8a71c23514f5c487089243aae7d4a3
Siri Frisli <siri.f@hotmail.com>
2024-06-12 10:22:23 +0200

    Added coffee shop files

---
+++ coffee_shop_module/coffee_customer.py
@@ -0,0 +1,6 @@
+import coffee_shop
+
+print("Welcome to", coffee_shop.shop_name)
+print("Available sizes:", coffee_shop.coffee_sizes)
+print("Available roasts:", coffee_shop.coffee_roasts)
+
---
+++ coffee_shop_module/coffee_shop.py
@@ -0,0 +1,26 @@
+"""
+Testing the coffee shop exercise from the Runestone Academy
+"""
+
+# Setting the name for the variables to be used
+shop_name = "Siri's Coffee Shop"
+coffee_sizes = ["small", "medium", "large"]
+coffee_roasts = ["hot chocolate", "light", "medium", "dark", "espresso"]
+
+def order_coffee(size, roast):
+    """
+    Take an order fom a user
+    :param size: a strong containing one of the coffee_sizes
+    :param roast: a string containing one of the coffee_roasts
+    :return: a message about the coffee order
+    """
+    return "Here's your {} coffee roasted {}".format(size, roast)
+
+def add_milk_please(fat_content):
+    """
+    Pretend like we're adding some milk to a coffee
+    :param fat_content: a string or integer containing the milkfat content
+    :return: a message about having added the milk
+    """
+    return "I've added the {}% milk".format(fat_content)
+
